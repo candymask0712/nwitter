1aa52c92f2f7012cdd439971c7ba338590cb7478
김준영 <>
2021-08-27 13:19:02 +0900

    1.0 Initializtion

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyFPZmSAkQx73_WfQAtMBXcokDBM1Voyc",
+  authDomain: "nwitter-7e904.firebaseapp.com",
+  projectId: "nwitter-7e904",
+  storageBucket: "nwitter-7e904.appspot.com",
+  messagingSenderId: "893886048255",
+  appId: "1:893886048255:web:e8c7c7959c89aab1e669ca"
+};
+
+export default firebase.initializeApp(firebaseConfig);
